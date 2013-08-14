590fb8baf77f646cebf3b417ebea678d7093f32f
Wesley Brooks <brooks.wesley@gmail.com>
2013-08-14 08:13:43 -0500

    allowed for writing to string as well as file

--- slidedeck/render.py
+++ slidedeck/render.py
@@ -37,39 +37,49 @@ DECK_SETTINGS_RE = {
 #############################################################################
 
 
-def process_slides(markdown_fn, output_fn, template_fn):
+def process(markdown_fn, template_fn):
     if not os.path.exists(markdown_fn):
         raise OSError('The markdown file "%s" could not be found.' % markdown_fn)
 
+    
+    md = codecs.open(markdown_fn, encoding='utf8').read()
+
+    md, settings = parse_deck_settings(md)
+
+    md_slides = md.split('\n---\n')
+    print("Compiled {:d} slides.".format(len(md_slides)))
+
+    slides = []
+    # Process each slide separately.
+    for md_slide in md_slides:
+        slide = {}
+        sections = md_slide.split('\n\n')
+        # Extract metadata at the beginning of the slide (look for key: value)
+        # pairs.
+        metadata_section = sections[0]
+        metadata = parse_metadata(metadata_section)
+        slide.update(metadata)
+        remainder_index = metadata and 1 or 0
+        # Get the content from the rest of the slide.
+        content_section = '\n\n'.join(sections[remainder_index:])
+        html = markdown.markdown(content_section)
+        slide['content'] = postprocess_html(html, metadata)
+
+        slides.append(slide)
+
+    template = jinja2.Template(open(template_fn).read())
+    return template.render(locals())
+
+
+def write_slides(slidestring)
     with codecs.open(output_fn, 'w', encoding='utf8') as outfile:
-        md = codecs.open(markdown_fn, encoding='utf8').read()
+        outfile.write(slidestring)
 
-        md, settings = parse_deck_settings(md)
 
-        md_slides = md.split('\n---\n')
-        print("Compiled {:d} slides.".format(len(md_slides)))
-
-        slides = []
-        # Process each slide separately.
-        for md_slide in md_slides:
-            slide = {}
-            sections = md_slide.split('\n\n')
-            # Extract metadata at the beginning of the slide (look for key: value)
-            # pairs.
-            metadata_section = sections[0]
-            metadata = parse_metadata(metadata_section)
-            slide.update(metadata)
-            remainder_index = metadata and 1 or 0
-            # Get the content from the rest of the slide.
-            content_section = '\n\n'.join(sections[remainder_index:])
-            html = markdown.markdown(content_section)
-            slide['content'] = postprocess_html(html, metadata)
-
-            slides.append(slide)
-
-        template = jinja2.Template(open(template_fn).read())
-
-        outfile.write(template.render(locals()))
+def process_slides(markdown_fn, output_fn, template_fn):
+    slides = process(markdown_fn, template_fn)
+    write_slides(slides)
+    
 
 
 def parse_deck_settings(md):
